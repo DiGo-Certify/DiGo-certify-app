f99928ce01f0f44b41d5b10fec83547459b4e0c7
49513 - Diogo Rodrigues <a49513@alunos.isel.pt>
2024-04-30 11:59:24 +0100

    Created first background generic to be used in all screens. Refactored the sign-up screen

--- code/app/(auth)/sign-up.jsx
+++ code/app/(auth)/sign-up.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Image, Text, Alert } from 'react-native';
+import { View, StyleSheet, Image, Text, Alert, Dimensions } from 'react-native';
 import Colors from '@/constants/colors';
 import { router } from 'expo-router';
 import FormField from '@/components/FormField';
 import ClickableText from '@/components/ClickableText';
 import ActionButton from '@/components/ActionButton';
+import Background from '@/components/Background';
+import Images from '@/constants/images';
+
+const windowWidth = Dimensions.get('window').width * 0.325;
 
 function SignUp() {
     const [isSubmitting, setSubmitting] = useState(false);
@@ -40,80 +44,98 @@ function SignUp() {
     };
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.registerText}>Register</Text>
-            <FormField
-                label="Full Name"
-                icon="account"
-                onChange={text => setForm({ ...form, username: text })}
-                style={styles.inputField}
-            />
-            <FormField
-                label="Email"
-                icon="email"
-                onChange={text => setForm({ ...form, email: text })}
-                style={styles.inputField}
-            />
-            <FormField
-                label="Password"
-                icon="lock"
-                onChange={text => setForm({ ...form, password: text })}
-                secure={true}
-                style={styles.inputField}
-            />
-            <FormField
-                label="Confirm Password"
-                icon="lock"
-                onChange={text => setForm({ ...form, confirmPassword: text })}
-                secure={true}
-                style={styles.inputField}
-            />
-            <ClickableText text="Forgot Password?" onPress={() => {}} style={styles.forgotPasswordText} />
-            <ActionButton
-                text="Register"
-                onPress={handleSubmit}
-                buttonStyle={styles.registerButton}
-                textStyle={styles.registerButtonText}
-                isLoading={isSubmitting}
-                color={Colors.green}
-            />
-        </View>
+        <Background
+            header={
+                // Image with app logo and a circle around it
+                <View style={styles.header}>
+                    <Image source={Images.splashScreenImage} style={styles.headerImage} />
+                </View>
+            }
+            body={
+                <View>
+                    <Text style={styles.registerText}>Register</Text>
+                    <FormField
+                        label="Full Name"
+                        icon="account"
+                        onChange={text => setForm({ ...form, username: text })}
+                        style={styles.inputField}
+                    />
+                    <FormField
+                        label="Email"
+                        icon="email"
+                        onChange={text => setForm({ ...form, email: text })}
+                        style={styles.inputField}
+                    />
+                    <FormField
+                        label="Password"
+                        icon="lock"
+                        onChange={text => setForm({ ...form, password: text })}
+                        secure={true}
+                        style={styles.inputField}
+                    />
+                    <FormField
+                        label="Confirm Password"
+                        icon="lock"
+                        onChange={text => setForm({ ...form, confirmPassword: text })}
+                        secure={true}
+                        style={styles.inputField}
+                    />
+                </View>
+            }
+            footer={
+                <View style={styles.footer}>
+                    <ClickableText text="Forgot Password?" onPress={() => {}} style={styles.forgotPasswordText} />
+                    <ActionButton
+                        text="Register"
+                        onPress={handleSubmit}
+                        buttonStyle={styles.registerButton}
+                        textStyle={styles.registerButtonText}
+                        isLoading={isSubmitting}
+                        color={Colors.green}
+                    />
+                </View>
+            }
+        />
     );
 }
 
 export default SignUp;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: Colors.backgroundColor,
+    header: {
+        // Circle with the image inside
+        aspectRatio: 1,
+        borderRadius: Dimensions.get('window').width * 0.325,
+        borderWidth: 5,
+        width: '65%',
+        height: '65%',
+    },
+    headerImage: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain',
     },
     inputField: {
         width: '85%',
-        marginTop: 24,
+        marginTop: 20,
         borderRadius: 10,
         borderBottomWidth: 2,
         borderBottomColor: Colors.sonicSilver,
     },
     registerText: {
-        marginTop: 90,
         fontSize: 40,
         fontWeight: '900',
+        alignSelf: 'flex-start',
+        marginTop: -150,
     },
     forgotPasswordText: {
         textDecorationLine: 'underline',
         alignSelf: 'flex-end',
-        marginTop: 20,
-        marginRight: 30,
-        padding: 30,
         fontSize: 20,
     },
     registerButton: {
         alignSelf: 'flex-end',
-        marginTop: 55,
-        marginRight: 20,
+        marginTop: 20,
         borderRadius: 10,
     },
 
@@ -123,4 +145,7 @@ const styles = StyleSheet.create({
         fontFamily: 'Poppins-Bold',
         color: '#FFF',
     },
+    footer: {
+        marginTop: -100
+    }
 });
--- code/constants/images.js
+++ code/constants/images.js
@@ -1,9 +1,11 @@
 import wallet from '../assets/images/wallet.png';
 import certificate from '../assets/images/certificate.png';
 import mockupProfileImage from '../assets/images/mockup-profile.png';
+import splashScreenImage from '../assets/images/splash.png';
 
 export default {
     wallet,
     certificate,
     mockupProfileImage,
+    splashScreenImage,
 };
